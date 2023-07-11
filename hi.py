f104923bfabcfdd71d6eef819c08609629c6c54b
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-11 09:27:24 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,6 +1,8 @@
 import telebot
 
 API_TOKEN = '5854990217:AAHljj4XJGdtUnp8fFdoEXBm9EviDNdH7WM'  # Replace with your actual API token
+WEBHOOK_URL = 'https://drab-ruby-seal-hose.cyclic.app/'  # Replace with your webhook URL
+
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -13,4 +15,9 @@ def handle_start(message):
         else:
             bot.reply_to(message, "No start value found in the link.")
 
-bot.polling()
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
+
+    # Start the bot's polling
+    bot.polling()
