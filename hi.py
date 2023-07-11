71117b6332a8de6e793f7855c697087dfad58fc6
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-11 09:35:23 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -3,11 +3,12 @@ from flask import Flask, request
 
 API_TOKEN = '5854990217:AAHljj4XJGdtUnp8fFdoEXBm9EviDNdH7WM'  # Replace with your actual API token
 WEBHOOK_URL = 'https://drab-ruby-seal-hose.cyclic.app/'  # Replace with your webhook URL
+WEBHOOK_PATH = '/webhook'  # Replace with your desired webhook path
 
 bot = telebot.TeleBot(API_TOKEN)
 app = Flask(__name__)
 
-@app.route('/YOUR_WEBHOOK_PATH', methods=['POST'])
+@app.route(WEBHOOK_PATH, methods=['POST'])
 def webhook():
     update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
     bot.process_new_updates([update])
