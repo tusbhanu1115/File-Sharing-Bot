f3a60d9ce09b0c5cc87981269e036b714e437899
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-11 09:38:22 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,18 +1,11 @@
 import telebot
-from flask import Flask, request
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 API_TOKEN = '5854990217:AAHljj4XJGdtUnp8fFdoEXBm9EviDNdH7WM'  # Replace with your actual API token
 WEBHOOK_URL = 'https://drab-ruby-seal-hose.cyclic.app/'  # Replace with your webhook URL
 WEBHOOK_PATH = '/webhook'  # Replace with your desired webhook path
 
 bot = telebot.TeleBot(API_TOKEN)
-app = Flask(__name__)
-
-@app.route(WEBHOOK_PATH, methods=['POST'])
-def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    bot.process_new_updates([update])
-    return 'OK', 200
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -24,9 +17,26 @@ def handle_start(message):
         else:
             bot.reply_to(message, "No start value found in the link.")
 
+class WebhookHandler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        update_data = self.rfile.read(content_length)
+        update = telebot.types.Update.de_json(update_data.decode('utf-8'))
+        bot.process_new_updates([update])
+        self._set_response()
+
+def run_server():
+    server_address = ('', 3000)  # Update port number if needed
+    httpd = HTTPServer(server_address, WebhookHandler)
+    httpd.serve_forever()
+
 if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=WEBHOOK_URL)
 
-    # Run the Flask application
-    app.run(host='0.0.0.0', port=3000)
+    run_server()
